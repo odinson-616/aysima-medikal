1a33ca8b1b13cc88ed381a301dd55b7b2eeddf06
odinson-616 <murathankrt@gmail.com>
2026-01-29 21:05:32 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 const SUPABASE_URL = 'https://eqpioawtdwkaeuxpfspt.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcGlvYXd0ZHdrYWV1eHBmc3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTk3OTksImV4cCI6MjA1Mjg5NTc5OX0.CvcbovBdNk9gLKZ4W-WeyQkPUIf6W55D6M7QIcTzIxs';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcGlvYXd0ZHdrYWV1eHBmc3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2Mjk2ODUsImV4cCI6MjA4NTIwNTY4NX0.TIOQvOaSTlcd8xs6lbLKJYPkJgzGGTHY1MPb9BahAWs';
 
 // Supabase'i window üzerinden başlatıyoruz (Hata almamak için kritik)
 if (window.supabase && typeof window.supabase.createClient === 'function') {
