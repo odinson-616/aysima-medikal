df90c5be87c2a245728f923f1527379c03f64938
odinson-616 <murathankrt@gmail.com>
2026-01-30 07:10:49 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,7 @@
 const SUPABASE_URL = "https://eqpioawtdwkaeuxpfspt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcGlvYXd0ZHdrYWV1eHBmc3B0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2Mjk2ODUsImV4cCI6MjA4NTIwNTY4NX0.TIOQvOaSTlcd8xs6lbLKJYPkJgzGGTHY1MPb9BahAWs";
+const SUPABASE_ANON_KEY = "ANON_KEY";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
-
-console.log("✅ Supabase bağlantısı hazır:", window.supabaseClient);
