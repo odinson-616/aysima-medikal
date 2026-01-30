492e02f129479f39175f1ea85e244b22cbbe64d6
odinson-616 <murathankrt@gmail.com>
2026-01-30 08:32:37 +0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://eqpioawtdwkaeuxpfspt.supabase.co";
-const SUPABASE_ANON_KEY = "ANON_KEY";
-
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
